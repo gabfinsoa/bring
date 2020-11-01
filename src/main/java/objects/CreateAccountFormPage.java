3775c77bcf56e25e16f1dfe5bc41f138bffc3e48
gabfinsoa <gabfinsoa@gmail.com>
2020-11-01 15:58:19 -0300

    Create_Firt_Objects

---
+++ src/main/java/objects/CreateAccountFormPage.java
@@ -0,0 +1,31 @@
+package objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Utils;
+
+public class CreateAccountFormPage extends Utils {
+
+    public CreateAccountFormPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "ap_customer_name")
+    public WebElement inpUserNameCreateAccount;
+
+    @FindBy(id = "ap_email")
+    public WebElement inpEmailCreateAccount;
+
+    @FindBy(id = "ap_password")
+    public WebElement inpPasswordCreateAccount;
+
+    @FindBy(id = "ap_password_check")
+    public WebElement inpPasswordCheckCreateAccount;
+
+    @FindBy(id = "continue")
+    public WebElement btnContinueCreateAccount;
+
+}
---
+++ src/main/java/objects/HomePageObjects.java
@@ -0,0 +1,19 @@
+package objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Utils;
+
+public class HomePageObjects extends Utils {
+
+    public HomePageObjects(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "nav-link-accountList")
+    public WebElement btnHomePageCuentaYListas;
+
+}
